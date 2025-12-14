0b8b374f79e0c1828b77ff78009b21f3b903a749
Ahmed Zoha <ahmedzoha@Ahmeds-MacBook-Pro.local>
2025-12-14 11:13:26 +0000

    Finalize FLAIR page rename and clean references

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
   site: 'https://www.ahmedzoha.com',
+  redirects: {
+    '/flair': '/flair',
+    '/flair/': '/flair/',
+  },
 });
+
+
